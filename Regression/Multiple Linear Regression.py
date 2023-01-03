b34e0b929c7b46fad995948949a351290044a363
Hamide Meraj <Hamidemeraj@gmail.com>
2023-01-03 08:29:28 +0330

    Regression

---
+++ Regression/Multiple Linear Regression.py
@@ -0,0 +1,59 @@
+"""
+Multiple Linear Regression
+"""
+# Import Libraries 
+from sklearn.datasets import load_boston
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
+import math 
+
+# Load Data
+Boston = load_boston()
+Boston_Data = pd.DataFrame(data=Boston.data, columns=Boston.feature_names)
+Boston_Data['Target'] = Boston.target
+
+# Splitting X , Y 
+x = Boston_Data.iloc[:,:13]
+y = Boston_Data.iloc[:, 13]
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, train_size=0.75, random_state=70)
+
+# Normalization 
+Sc = MinMaxScaler(feature_range=(0,1))
+X_train = Sc.fit_transform(X_train)
+X_test = Sc.fit_transform(X_test)
+y_train = y_train.values.reshape(-1,1)
+y_train = Sc.fit_transform(y_train)
+
+# Creating a Linear Model
+MLR = LinearRegression()
+MLR.fit(X_train,y_train)
+
+# These are Normalized Values
+y_predicted_MLR = MLR.predict(X_test)
+# Converting to Actual Values
+y_predicted_MLR = Sc.inverse_transform(y_predicted_MLR)
+
+# Evaluating Model
+mae = mean_absolute_error(y_test, y_predicted_MLR)
+mse = mean_squared_error(y_test, y_predicted_MLR)
+rmse = math.sqrt(mse)
+mean_absolute_percentage_error(y_test, y_predicted_MLR)
+R2 = r2_score(y_test, y_predicted_MLR)
+
+# Define Metrics without Using Libraries
+def Mean_Absolute_Percentage_Error(y_true,y_pred):
+    y_true , y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true-y_pred)/y_true))*100
+
+mape = Mean_Absolute_Percentage_Error(y_test,y_predicted_MLR)
+
---
+++ Regression/polynomial Regression.py
@@ -0,0 +1,37 @@
+"""
+Polynomial Linear Regression
+"""
+# Import Libraries
+from sklearn.datasets import load_boston
+import pandas as pd 
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from  sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+
+# Load Data
+Boston = load_boston()
+#Extract one Feature for polynomial linear regression
+Boston_Data =  pd.DataFrame(Boston.data, columns=Boston.feature_names)
+x = Boston_Data.iloc[:,4]
+y = Boston_Data.iloc[:,-1]
+x = x.to_numpy()
+y = y.to_numpy()
+
+# Use random state like MLR to compare your results  
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, train_size=0.75, random_state=70)
+
+# Create a Polynomial Feature from Selected Feature
+Poly_p = PolynomialFeatures(degree = 2)
+X_train = X_train.reshape(-1,1)
+Poly_X = Poly_p.fit_transform(X_train)
+
+# Create a Linear Model
+LR = LinearRegression()
+PLR = LR.fit(X_train, y_train)
+X_test = X_test.reshape(-1,1)
+y_predicted_PLR = LR.predict(X_test)
+
+# Evaluate Model
+R2 = r2_score(y_test, y_predicted_PLR)
